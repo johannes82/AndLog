933a7a6ad74154aff17d4a0541235a6dd7674a6d
Johannes Steudle <external.johannes.steudle@bosch-softtec.com>
2014-04-24 17:21:15 +0200

    Improve regex and key

--- and_log.py
+++ and_log.py
@@ -7,15 +7,15 @@ __author__ = 'johannes82'
 # pattern = r'(([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d)(\s*|:)(\d*:\s*\d*\s)(V|I|W|D|E)/($|[^\[$\n]*))'
 
 global info_pattern
-info_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(I)/($|[^\[$\n]*))'
+info_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(I)/.*[^\n])'
 global verbose_pattern
-verbose_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(V)/($|[^\[$\n]*))'
+verbose_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(V)/.*[^\n])'
 global warning_pattern
-warning_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(W)/($|[^\[$\n]*))'
+warning_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(W)/.*[^\n])'
 global debug_pattern
-debug_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(D)/($|[^\[$\n]*))'
+debug_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(D)/.*[^\n])'
 global error_pattern
-error_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(E)/($|[^\[$\n]*))'
+error_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(E)/.+[^\n])'
 
 
 def plugin_loaded():
@@ -55,18 +55,18 @@ class EnableHighlightingCommand(sublime_plugin.TextCommand):
 		else:
 			Pref.highlighted = False
 			print ("Highlighting", Pref.highlighted)
-			self.view.erase_regions("AndLog" + info_pattern)
-			self.view.erase_regions("AndLog" + verbose_pattern)
-			self.view.erase_regions("AndLog" + warning_pattern)
-			self.view.erase_regions("AndLog" + debug_pattern)
-			self.view.erase_regions("AndLog" + error_pattern)
+			self.view.erase_regions(info_pattern)
+			self.view.erase_regions(verbose_pattern)
+			self.view.erase_regions(warning_pattern)
+			self.view.erase_regions(debug_pattern)
+			self.view.erase_regions(error_pattern)
 
 
 	def highlight_text(self, view, pattern, color):
 		print ("Highlighting ", pattern, "with color", color)
 		regions = []
 		regions += view.find_all(pattern, False)
-		key = "AndLog" + pattern
+		key = pattern
 		view.add_regions(key, regions, color, "", True)
 
 
