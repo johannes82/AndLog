3a85c0f48ce44ca988dadb2977b72bc78e07fcb1
Johannes Steudle <external.johannes.steudle@bosch-softtec.com>
2014-04-28 15:16:54 +0200

    Simplify regex handling, add first custom filter

--- and_log.py
+++ and_log.py
@@ -6,16 +6,19 @@ __author__ = 'johannes82'
 # global pattern
 # pattern = r'(([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d)(\s*|:)(\d*:\s*\d*\s)(V|I|W|D|E)/($|[^\[$\n]*))'
 
+base_pattern = '^(\[\s)*[0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s'
 global info_pattern
-info_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(I)/.*[^\n])'
+info_pattern = r'(' + base_pattern + '(I)/.*[^\n])'
 global verbose_pattern
-verbose_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(V)/.*[^\n])'
+verbose_pattern = r'(' + base_pattern + '(V)/.*[^\n])'
 global warning_pattern
-warning_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(W)/.*[^\n])'
+warning_pattern = r'(' + base_pattern + '(W)/.*[^\n])'
 global debug_pattern
-debug_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(D)/.*[^\n])'
+debug_pattern = r'(' + base_pattern + '(D)/.*[^\n])'
 global error_pattern
-error_pattern = r'([0-1][0-9]-[0-3][\d]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\.\d\d\d(\s*|:)\d*:\s*\d*\s(E)/.+[^\n])'
+error_pattern = r'(' + base_pattern + '(E)/.+[^\n])'
+global myspin_pattern
+myspin_pattern = r'(' + base_pattern + '(.{1})/MySpin.+[^\n])'
 
 
 def plugin_loaded():
@@ -28,7 +31,12 @@ def plugin_loaded():
 class Pref:
 	def load(self):
 		settings = sublime.load_settings('AndLog.sublime-settings')
-		Pref.highlighting_mode   = settings.get('highlighting_mode', "verbose")
+		Pref.highlighting_info   = settings.get('highlighting_info', False)
+		Pref.highlighting_verbose   = settings.get('highlighting_verbose', False)
+		Pref.highlighting_warning   = settings.get('highlighting_warning', False)
+		Pref.highlighting_debug   = settings.get('highlighting_debug', True)
+		Pref.highlighting_error   = settings.get('highlighting_error', True)
+		Pref.highlighting_myspin   = settings.get('highlighting_myspin', True)
 		Pref.highlighted         = False
 		Pref.color_scope_info    = settings.get('color_scope_info', "comment")
 		Pref.color_scope_verbose = settings.get('color_scope_verbose', "support.type.exception")
@@ -47,11 +55,18 @@ class EnableHighlightingCommand(sublime_plugin.TextCommand):
 		if not (Pref.highlighted):
 			Pref.highlighted = True
 			print ("Highlighting", Pref.highlighted)
-			# self.highlight_text(self.view, info_pattern, Pref.color_scope_info)
-			self.highlight_text(self.view, verbose_pattern, Pref.color_scope_verbose)
-			self.highlight_text(self.view, warning_pattern, Pref.color_scope_warning)
-			self.highlight_text(self.view, debug_pattern, Pref.color_scope_debug)
-			self.highlight_text(self.view, error_pattern, Pref.color_scope_error)
+			if (Pref.highlighting_info):
+				self.highlight_text(self.view, info_pattern, Pref.color_scope_info)
+			if (Pref.highlighting_verbose):
+				self.highlight_text(self.view, verbose_pattern, Pref.color_scope_verbose)
+			if (Pref.highlighting_warning):
+				self.highlight_text(self.view, warning_pattern, Pref.color_scope_warning)
+			if (Pref.highlighting_debug):
+				self.highlight_text(self.view, debug_pattern, Pref.color_scope_debug)
+			if (Pref.highlighting_error):
+				self.highlight_text(self.view, error_pattern, Pref.color_scope_error)
+			if (Pref.highlighting_myspin):
+				self.highlight_text(self.view, myspin_pattern, Pref.color_scope_error)
 		else:
 			Pref.highlighted = False
 			print ("Highlighting", Pref.highlighted)
@@ -60,6 +75,7 @@ class EnableHighlightingCommand(sublime_plugin.TextCommand):
 			self.view.erase_regions(warning_pattern)
 			self.view.erase_regions(debug_pattern)
 			self.view.erase_regions(error_pattern)
+			self.view.erase_regions(myspin_pattern)
 
 
 	def highlight_text(self, view, pattern, color):
